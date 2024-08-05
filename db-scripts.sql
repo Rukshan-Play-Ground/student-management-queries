523800ed2f11c1fa80e70b1764604440e37ec89c
Tharindu Rukshan <tharindurukshan709@gmail.com>
2024-08-05 21:39:58 +0530

    complete 7

--- db-scripts.sql
+++ db-scripts.sql
@@ -5,4 +5,5 @@ CREATE TABLE student_exam(
     CONSTRAINT pk_student_exam PRIMARY KEY (student_id, exam_code),
     CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES student (id),
     CONSTRAINT fk_exam FOREIGN KEY (exam_code) REFERENCES exam (exam_code)
+
 );
