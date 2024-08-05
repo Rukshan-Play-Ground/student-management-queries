df45b241366cfee4f0ad88a7a3c4017ce07379fe
Ravindya Jayasooriya <ravindyadivanjani@gmail.com>
2024-08-05 22:23:02 +0530

    Update 1

--- db-scripts.sql
+++ db-scripts.sql
@@ -1,8 +1,8 @@
 CREATE TABLE program_module(
+
                                program_id VARCHAR(10) NOT NULL,
                                module_id VARCHAR(10) NOT NULL ,
                                CONSTRAINT pk_program_module PRIMARY KEY (program_id, module_id),
                                CONSTRAINT fk_program FOREIGN KEY (program_id) REFERENCES program (id),
                                CONSTRAINT fk_module FOREIGN KEY (module_id) REFERENCES module (id)
-
 );
